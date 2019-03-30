ecc32305bfd9becf1427de75e70d4b6045353307
axixnix <henryirons@yahoo.com>
2019-03-31 00:24:50 +0100

    done some basic work already,last thing I did before adding to git was to install bootstrap

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Contact from './components/Contact'
+import Header from './components/Header'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <Header branding='Contact Manager' />
+        <Contact name='John Doe' email='jdoe@yahoo.com' phone='555-555-555' />
+        <Contact name='Karen Doe' email='kdoe@yahoo.com' phone='333-555-555' />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Contact.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Contact extends Component {
+  render () {
+    const {name, email, phone} = this.props
+
+    return (
+      <div>
+        <h4>{name}</h4>
+        <ul>
+          <li>
+            {email}
+          </li>
+          <li>
+            {phone}
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+Contact.propTypes = {name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+phone: PropTypes.string.isRequired}
+
+Contact.defaultProps = {name: 'Hokage',email: 'talk2mehokage@konoha.com'}
+
+export default Contact
