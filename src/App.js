19f74518d84dd02b48e584e25edbdf6fa9cdd1f0
axixnix <henryirons@yahoo.com>
2019-03-31 00:57:47 +0100

    turned the returning div in Header to a nav bar, but it does not look like it is supposed to

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import Contact from './components/Contact'
 import Header from './components/Header'
-import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
 class App extends Component {
   render () {
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,20 @@ const Header = (props) => {
   const {branding} = props
 
   return (
-    <div>
-      <h1>{branding}</h1>
-    </div>
+    <nav className='navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0'>
+      <div className='container'>
+        <a href='/' className='navbar-bran'>
+          {branding}
+        </a>
+        <div>
+          <ul className='navbar-brand mr-auto'>
+            <li className='nav-item'>
+              <a href='/' className='nav-link'>Home</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 Header.propTypes = {branding: propTypes.string.isRequired}
