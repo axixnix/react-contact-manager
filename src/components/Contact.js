6b8b5b47daa2d0cea7bac77b6ff8f5c6d1f25a74
axixnix <henryirons@yahoo.com>
2019-04-02 22:16:38 +0100

    toggle effect was not working properly initially because in my setState my new state value was bangthisdotshowcontactinfo instead of bangthisdotstate.showcontactinfo

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
-  state = {};
-
-  onShowClick = e => {};
+  state = { showContactInfo: false };
 
   onDeleteClick = () => {
     this.props.deleteClickHandler();
@@ -12,13 +10,16 @@ class Contact extends Component {
 
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
 
     return (
       <div className="card card-body mb-3">
         <h4>
-          {name}{" "}
+          {name}
           <i
-            onClick={this.onShowClick}
+            onClick={() => {
+              this.setState({ showContactInfo: !this.state.showContactInfo });
+            }}
             className="fas fa-sort-down"
             style={{ cursor: "pointer" }}
           />
@@ -28,10 +29,12 @@ class Contact extends Component {
             style={{ cursor: "pointer", float: "right", color: "red" }}
           />
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">{email}</li>
-          <li className="list-group-item">{phone}</li>
-        </ul>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">{email}</li>
+            <li className="list-group-item">{phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
