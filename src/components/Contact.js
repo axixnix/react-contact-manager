48f0235e5441154c0cd346328f8ac265327f0917
axixnix <henryirons@yahoo.com>
2019-04-05 00:06:34 +0100

    delete functionality via reducer is complete, on line 34 id had to come before dispatch I wonder why

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,36 +5,47 @@ import { Consumer } from "../context";
 class Contact extends Component {
   state = { showContactInfo: false };
 
-  onDeleteClick = () => {};
+  onDeleteClick = (id, dispatch) => {
+    dispatch({ type: "DELETE_CONTACT", payload: id });
+  };
 
   render() {
-    const { name, email, phone } = this.props.contact;
+    const { name, email, phone, id } = this.props.contact;
     const { showContactInfo } = this.state;
 
     return (
-      <div className="card card-body mb-3">
-        <h4>
-          {name}
-          <i
-            onClick={() => {
-              this.setState({ showContactInfo: !this.state.showContactInfo });
-            }}
-            className="fas fa-sort-down"
-            style={{ cursor: "pointer" }}
-          />
-          <i
-            onClick={this.onDeleteClick}
-            className="fas fa-times"
-            style={{ cursor: "pointer", float: "right", color: "red" }}
-          />
-        </h4>
-        {showContactInfo ? (
-          <ul className="list-group">
-            <li className="list-group-item">{email}</li>
-            <li className="list-group-item">{phone}</li>
-          </ul>
-        ) : null}
-      </div>
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h4>
+                {name}
+                <i
+                  onClick={() => {
+                    this.setState({
+                      showContactInfo: !this.state.showContactInfo
+                    });
+                  }}
+                  className="fas fa-sort-down"
+                  style={{ cursor: "pointer" }}
+                />
+                <i
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                  className="fas fa-times"
+                  style={{ cursor: "pointer", float: "right", color: "red" }}
+                />
+              </h4>
+              {showContactInfo ? (
+                <ul className="list-group">
+                  <li className="list-group-item">{email}</li>
+                  <li className="list-group-item">{phone}</li>
+                </ul>
+              ) : null}
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,37 +1,27 @@
-import React, { Component } from "react";
-import Contact from "./Contact";
-import { Consumer } from "../context";
+import React, { Component } from 'react'
+import Contact from './Contact'
+import { Consumer } from '../context'
 
 class Contacts extends Component {
-  deleteContact = id => {
-    const { contacts } = this.state;
 
-    const newContacts = contacts.filter(contact => contact.id !== id);
 
-    this.setState({ contacts: newContacts });
-  };
-
-  render() {
+  render () {
     return (
       <Consumer>
         {value => {
-          console.log(value);
-          const { contacts } = value;
-          return (
-            <React.Fragment>
-              {contacts.map(contact => (
-                <Contact
-                  key={contact.id}
-                  contact={contact}
-                  deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-                />
-              ))}
-            </React.Fragment>
-          );
-        }}
+           console.log(value)
+           const { contacts } = value
+           return (
+             <React.Fragment>
+               {contacts.map(contact => (
+                  <Contact key={contact.id} contact={contact} />
+                ))}
+             </React.Fragment>
+           )
+         }}
       </Consumer>
-    );
+    )
   }
 }
 
-export default Contacts;
+export default Contacts
